80725dc52370379aa983b53ae7c043630d71a482
Anup Bhandari <anup.b@HOBB.COM>
2024-09-12 14:08:53 +0530

    Added text geometry example

---
+++ 3d-text-demo/text3d.js
@@ -0,0 +1,102 @@
+import * as THREE from 'three'
+import { TextGeometry } from 'text3D'
+import { OrbitControls } from 'orbit'
+import { LineMaterial } from 'lineMaterial'
+import { ENVMAP_PATH } from 'global-constants'
+import { FontLoader } from 'loader';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 3)
+const renderer = new THREE.WebGLRenderer({antialias: true});
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+scene.background = new THREE.TextureLoader().load(ENVMAP_PATH);
+
+let mainLine 
+let dimension 
+
+const loader = new FontLoader()
+loader.load('Roobert Medium_Regular.json', f=>{
+    const dimensionLine = new THREE.Group()
+    
+    const textGeometry = new TextGeometry('40', {
+        font: f,
+        size: 0.02,
+        height: 0.01,
+        curveSegments: 16,
+        bevelEnabled: false,
+        bevelThickness: 0.0125,
+        bevelSize: 0.025,
+        bevelOffset: 0,
+        bevelSegments: 16
+    })
+    textGeometry.center()
+    const textMaterial = new THREE.MeshBasicMaterial({color: new THREE.Color(1, 1, 1)})
+    const text = new THREE.Mesh(textGeometry, textMaterial)
+    text.position.set(0, 0, 0)
+    dimensionLine.attach(text)
+
+    const plane = new THREE.Mesh(new THREE.PlaneGeometry(0.1, 0.05), new THREE.MeshBasicMaterial({color: new THREE.Color(0, 0, 0), side: THREE.DoubleSide}))
+    plane.position.set(0, 0, 0)
+    dimensionLine.attach(plane)
+
+    /* const lineMaterial = new LineMaterial({
+        color: new THREE.Color(1, 0, 0), 
+        vertexColors: true, 
+        linewidth: 5, 
+        gapSize: 1, 
+        dashed: false
+    }) */
+
+    const lines = new THREE.Group()
+
+    const lineMaterial = new THREE.LineBasicMaterial({
+        color: new THREE.Color(1, 0, 0), 
+        vertexColors: true, 
+        linewidth: 5
+    })
+    lineMaterial.linewidth = 10
+    lineMaterial.worldUnits = true
+    lineMaterial.needsUpdate = true
+
+    const points = [new THREE.Vector3(-0.2, 0, 0), new THREE.Vector3(0.2, 0, 0)]
+    
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+    const line = new THREE.Line(lineGeometry, lineMaterial)
+    //line.computeLineDistances()
+    lines.attach(line)
+
+    const endPoints = [new THREE.Vector3(0, -0.02, 0), new THREE.Vector3(0, 0.02, 0)]
+    const endLineGeometry = new THREE.BufferGeometry().setFromPoints(endPoints)
+    const endLine1 =  new THREE.Line(endLineGeometry, lineMaterial)
+    endLine1.position.set(-0.2, 0, 0)
+    lines.attach(endLine1)
+    
+    const endLine2 =  new THREE.Line(endLineGeometry, lineMaterial)
+    endLine2.position.set(0.2, 0, 0)
+    lines.attach(endLine2)
+
+    dimensionLine.attach(lines)
+
+    scene.add(dimensionLine)
+
+    mainLine = lines
+    dimension = dimensionLine
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    animate()
+})
+
+function animate() 
+{
+    requestAnimationFrame(animate)
+    /* if (mainLine != undefined)
+        mainLine.scale.x += 0.01
+    if (dimension != undefined)
+        dimension.position.y -= 0.01 */
+    renderer.render(scene, camera)
+}
